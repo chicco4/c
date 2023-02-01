0d981b9721c93cfa7b51a3a978945805e5de8fca
chiccotto4 <chiccotto4@gmail.com>
2023-02-01 17:42:38 +0100

    spacing

--- snake_labyrint/snake_labyrinth.h
+++ snake_labyrint/snake_labyrinth.h
@@ -12,28 +12,40 @@
 
 /**number of rows in the board*/
 int rows;
+
 /**number of cols in the board*/
 int cols;
+
 /**matrix of chars that represent the labyrinth*/
 char **board;
+
 /**is the game finished?*/
 int gameover;
+
 /**is the game won or lost?*/
 int win;
+
 /**game score, it starts at 1000*/
 int score;
+
 /**it rapresents the direction of the snake move*/
 int direction;
+
 /**lenght of the tail array*/
 int tail_lenght;
+
 /**number of usages of the drill*/
 int drill_usages;
+
 /**is the game in ai mode or in user input mode*/
 int ai_mode;
+
 /**array of moves*/
 int *moves;
+
 /**moves counter*/
 int moves_lenght;
+
 /**
  * @brief structure that represent a coordinate
  * @details made with two i and j integers
@@ -43,35 +55,45 @@ typedef struct
     int i;
     int j;
 } coordinates;
+
 /**i split the snake head from the tail to make implementation of the game clearer*/
 coordinates snake_head;
+
 /**array of coordinates that rappresent the tail of the snake*/
 coordinates *snake_tail;
 
 /**function that scanf rows, cols and the labyrinth*/
 void setup();
+
 /**function that implements the logic of the game every time the snake moves*/
 void logic();
+
 /**refresh the board and draw it*/
 void draw();
+
 /**
  * @brief simple function that changes the direction flag from user input
  * @details it uses the getChar() function;
  */
 void input();
+
 /**
  * @brief simple function that pick a random direction after checking if possible to make
  */
 void random_ai();
+
 /**
  * @brief function that allows the snake to follow the right wall of the labyrinth untill it
  * @attention not yet implemented
  */
 void wall_follow_ai();
+
 /**print ending massage according to the win flag, also print the score*/
 void ending();
+
 /**it frees the varius all the dinamically allocated memory*/
 void free_memory();
+
 /**
  * @brief main loop
  *
