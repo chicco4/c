853c30051adbe10f18410adef173d96ab8c5db69
chiccotto4 <chiccotto4@gmail.com>
2023-02-01 22:05:26 +0100

    aa

--- util.c
+++ util.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -16,6 +17,15 @@ int main(void)
     printf("a -> %d\n", a);
     printf("b -> %d\n", b);
     //printf("c -> %d\n", c);
+
+    int x = 10;
+    int y = 10;
+
+    printf("x -> %d\n", x++);
+    printf("x -> %d\n", x);
+
+    printf("y -> %d\n", ++y);
+    printf("y -> %d\n", y);
 
     return 0;
 }
