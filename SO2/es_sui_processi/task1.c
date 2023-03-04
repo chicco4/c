252d7e3e5de8d415e729e13b75a78b7ae805d6ad
chiccotto4 <chiccotto4@gmail.com>
2023-03-04 17:52:34 +0100

    refact

--- SO2/es_sui_processi/task1.c
+++ SO2/es_sui_processi/task1.c
@@ -5,12 +5,11 @@
 #include <string.h>
 #include <sys/wait.h>
 
+/*https://secgroup.dais.unive.it/teaching/laboratorio-sistemi-operativi/esercitazione-su-gestione-dei-processi/task-1-il-programma-da-craccare/*/
+
 int main()
 {
     pid_t ret = fork();
 
-    
-
     system("./checkPIN 01234");
-    
 }
